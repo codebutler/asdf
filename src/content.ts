e5fb984e6e5fa936a51706b411903fd9af6d2b42
Eric Butler <eric@maybern.com>
2023-09-22 14:59:59 -0400

    combobox support

--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,10 @@
 import { faker } from "@faker-js/faker";
 import userEvent from "@testing-library/user-event";
-import { difference, Pselector } from "./util";
-import { P, match } from "ts-pattern";
+import { difference, ensure, Pselector } from "./util";
+import { match, P } from "ts-pattern";
+import { fireEvent, waitFor } from "@testing-library/dom";
 
-const FORM_ELEMENT_SELECTOR = "input, select, textarea";
+const FORM_ELEMENT_SELECTOR = "input, select, textarea, *[role=combobox]";
 
 type FormElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
 
@@ -19,7 +20,7 @@ const fillElements = async () => {
 
   const fillUnseenElements = async () => {
     const allElements = new Set(
-      document.querySelectorAll<FormElement>(FORM_ELEMENT_SELECTOR)
+      document.querySelectorAll<FormElement>(FORM_ELEMENT_SELECTOR),
     );
     const newElements = difference(allElements, seenElements);
     if (newElements.size === 0) {
@@ -27,15 +28,20 @@ const fillElements = async () => {
     }
 
     for (const elem of newElements) {
-      seenElements.add(elem);
-      if (shouldSkipElement(elem)) {
-        continue;
+      try {
+        seenElements.add(elem);
+        if (shouldSkipElement(elem)) {
+          continue;
+        }
+        await match(elem)
+          .with(Pselector("[role=combobox]"), fillCombobox)
+          .with(P.instanceOf(HTMLInputElement), fillInput)
+          .with(P.instanceOf(HTMLSelectElement), fillSelect)
+          .with(P.instanceOf(HTMLTextAreaElement), fillTextArea)
+          .exhaustive();
+      } catch (e) {
+        console.warn("Error filling element", elem, e);
       }
-      await match(elem)
-        .with(P.instanceOf(HTMLInputElement), fillInput)
-        .with(P.instanceOf(HTMLSelectElement), fillSelect)
-        .with(P.instanceOf(HTMLTextAreaElement), fillTextArea)
-        .exhaustive();
     }
 
     // Check for any new elements that might have appeared
@@ -56,41 +62,44 @@ const fillInput = async (input: HTMLInputElement) => {
       }
     })
     .with(Pselector("[type=color]"), (input) =>
-      userEvent.type(input, faker.internet.color())
+      userEvent.type(input, faker.internet.color()),
     )
     .with(Pselector("[type=date], [type=datetime-local]"), (input) =>
       userEvent.type(
         input,
-        new Intl.DateTimeFormat(navigator.language).format(faker.date.future())
-      )
+        new Intl.DateTimeFormat(navigator.language).format(faker.date.future()),
+      ),
     )
     .with(Pselector("[type=email]"), (input) =>
-      userEvent.type(input, faker.internet.email())
+      userEvent.type(input, faker.internet.email()),
     )
     .with(Pselector("[type=month]"), (input) =>
-      userEvent.type(input, faker.date.month())
+      userEvent.type(input, faker.date.month()),
     )
     .with(Pselector("[type=number]"), (input) =>
       userEvent.type(
         input,
         faker.number
-          .int({ min: Number(input.min), max: Number(input.max) })
-          .toString()
-      )
+          .int({
+            min: Number(input.min),
+            max: Number(input.max),
+          })
+          .toString(),
+      ),
     )
     .with(Pselector("[type=password]"), (input) =>
-      userEvent.type(input, faker.internet.password())
+      userEvent.type(input, faker.internet.password()),
     )
     .with(Pselector("[type=radio]"), async (input) => {
       const allOptions = document.querySelectorAll<HTMLInputElement>(
-        `input[name="${input.name}"]`
+        `input[name="${input.name}"]`,
       );
       const isAnyOptionChecked = Array.from(allOptions).some(
-        (option) => option.checked
+        (option) => option.checked,
       );
       if (!isAnyOptionChecked) {
         await userEvent.click(
-          faker.helpers.arrayElement(Array.from(allOptions))
+          faker.helpers.arrayElement(Array.from(allOptions)),
         );
       }
     })
@@ -98,18 +107,26 @@ const fillInput = async (input: HTMLInputElement) => {
       userEvent.type(
         input,
         faker.number
-          .int({ min: Number(input.min), max: Number(input.max) })
-          .toString()
-      )
+          .int({
+            min: Number(input.min),
+            max: Number(input.max),
+          })
+          .toString(),
+      ),
     )
     .with(Pselector("input[type=search]"), (input) =>
       userEvent.type(
         input,
-        faker.lorem.sentence({ min: 1, max: 4 }).slice(0, -1)
-      )
+        faker.lorem
+          .sentence({
+            min: 1,
+            max: 4,
+          })
+          .slice(0, -1),
+      ),
     )
     .with(Pselector("[type=tel]"), (input) =>
-      userEvent.type(input, faker.phone.number())
+      userEvent.type(input, faker.phone.number()),
     )
     .with(Pselector("input[type=text]"), (input) =>
       match(input)
@@ -117,11 +134,10 @@ const fillInput = async (input: HTMLInputElement) => {
           userEvent.type(
             input,
             new Intl.DateTimeFormat(navigator.language).format(
-              faker.date.future()
-            )
-          )
+              faker.date.future(),
+            ),
+          ),
         )
-        .with(Pselector("[role=combobox]"), () => null) // FIXME: Add support for comboboxes
         .with(Pselector("[inputmode=decimal], [inputmode=numeric]"), (input) =>
           userEvent.type(
             input,
@@ -130,26 +146,48 @@ const fillInput = async (input: HTMLInputElement) => {
                 min: input.min ? Number(input.min) : 0,
                 max: input.max ? Number(input.max) : 50_000_000,
               })
-              .toString()
-          )
+              .toString(),
+          ),
         )
+        .with(Pselector("[name*=first_name]"), (input) =>
+          userEvent.type(input, faker.person.firstName()),
+        )
+        .with(Pselector("[name*=last_name]"), (input) =>
+          userEvent.type(input, faker.person.lastName()),
+        )
+        .with(Pselector("[name*=full_name]"), (input) =>
+          userEvent.type(input, faker.person.fullName()),
+        )
+        .with(Pselector("[name*=address]"), (input) =>
+          userEvent.type(input, faker.location.street()),
+        )
+        .with(Pselector("[name*=city]"), (input) =>
+          userEvent.type(input, faker.location.city()),
+        )
+
         .otherwise((input) =>
           userEvent.type(
             input,
-            faker.lorem.sentence({ min: 1, max: 4 }).slice(0, -1)
-          )
-        )
+            faker.lorem
+              .sentence({
+                min: 1,
+                max: 4,
+              })
+              .slice(0, -1),
+          ),
+        ),
     )
     .with(Pselector("input[type=time]"), (input) =>
       userEvent.type(
         input,
-        faker.date
-          .future()
-          .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-      )
+        faker.date.future().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      ),
     )
     .with(Pselector("input[type=url]"), (input) =>
-      userEvent.type(input, faker.internet.url())
+      userEvent.type(input, faker.internet.url()),
     )
     .with(Pselector("input[type=week]"), () => null) // FIXME
     .otherwise(() => null);
@@ -157,7 +195,7 @@ const fillInput = async (input: HTMLInputElement) => {
 
 const fillSelect = async (select: HTMLSelectElement) => {
   const options = select.querySelectorAll<HTMLOptionElement>(
-    "option:not([disabled]):not([hidden]):not([value=''])"
+    "option:not([disabled]):not([hidden]):not([value=''])",
   );
   if (options.length > 0) {
     const randomOption = faker.helpers.arrayElement(Array.from(options));
@@ -167,6 +205,41 @@ const fillSelect = async (select: HTMLSelectElement) => {
 
 const fillTextArea = async (textarea: HTMLTextAreaElement) => {
   await userEvent.type(textarea, faker.lorem.paragraph());
+};
+
+const fillCombobox = async (input: HTMLInputElement) => {
+  input.focus();
+  fireEvent.keyDown(input, {
+    key: "ArrowDown",
+    code: "ArrowDown",
+    keyCode: 40,
+    charCode: 40,
+  });
+
+  await waitFor(() =>
+    ensure(
+      input.getAttribute("aria-expanded") === "true",
+      "combobox not expanded",
+    ),
+  );
+
+  const listboxId = await waitFor(() =>
+    ensure(
+      input.getAttribute("aria-owns") || input.getAttribute("aria-controls"),
+      "combobox has no aria-owns or aria-controls",
+    ),
+  );
+
+  const listbox = await waitFor(() =>
+    ensure(document.getElementById(listboxId), "combobox has no listbox"),
+  );
+
+  const options = listbox.querySelectorAll<HTMLElement>(
+    "*[role=option], *[role=button], *[role=menuitem]",
+  );
+  ensure(options.length > 0, "combobox has no options");
+  const randomOption = faker.helpers.arrayElement(Array.from(options));
+  await userEvent.click(randomOption);
 };
 
 const shouldSkipElement = (elem: FormElement) => {
@@ -179,6 +252,7 @@ const shouldSkipElement = (elem: FormElement) => {
     elem.disabled ||
     (isInput && elem.type === "hidden") ||
     (!isRadio && elem.value) ||
-    (isInputOrTextArea && elem.readOnly)
+    (isInputOrTextArea && elem.readOnly) ||
+    elem.getAttribute("data-value")
   );
 };
--- src/util.ts
+++ src/util.ts
@@ -12,3 +12,10 @@ export const Pselector = (selector: string) =>
   P.when<HTMLElement, (input: HTMLElement) => boolean>((input) =>
     input.matches(selector)
   );
+
+export const ensure = <T>(reference: T | null | undefined, message?: string): T => {
+  if (reference === null || reference === undefined) {
+    throw new Error(message || 'Value cannot be null or undefined.');
+  }
+  return reference;
+};
